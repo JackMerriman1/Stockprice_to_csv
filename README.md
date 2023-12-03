528905ed61007635a55c08e7f2c3213546fd8da5
JackMerriman1 <jack.merriman2@gmail.com>
2023-12-03 19:19:20 +0000

    commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Stockprice_to_csv
- 
+This program will generate a CSV file viewable in Excel/Google sheets, located within this folder, with data regarding the prices of stocks.  All you need to do is follow the instrucitons below:
+
+How to use the program!
+1. Go to https://polygon.io/dashboard/api-keys
+	a. Create an account and generate an API Key, you will need to enter this into the Api Key field in the program
+2. Run the Stock_Price_To_CSV_EXE file
+3. Enter the corresponding information into each field, make sure the format is correct for the start and end date field otherwise it will not work.
+4. Go to the folder this program is located in to find your CSV file.
+
+NOTES: Every time you click Fetch Data the program will generate a new CSV file.
+I.E if you want multiple CSV files representing different ticker symbols just edit the ticker field and press Fetch Data, no need to restart the program. :)
---
+++ Stock_Price_To_CSV_PY.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import json
+import requests
+from IPython.display import display
+import time
+import sys
+import tkinter as tk
+
+def create_url(ticker_symbol, start_date, end_date, api_key):
+    
+    url = f"https://api.polygon.io/v2/aggs/ticker/{ticker_symbol.upper()}/range/1/day/{start_date}/{end_date}?apiKey={api_key}"
+    return url
+
+def create_dataframe(url):
+    
+    # Retrieve data in JSON format
+    results = requests.get(url).json()
+
+    # Create Dataframe
+    df = pd.DataFrame(results["results"])
+    renamed_df = df.rename(columns={'c': 'Close Price', 'h': 'Highest Price',
+                                    'l': 'Lowest Price', 'n': 'Number of Transactions',
+                                    'o': 'Opening Price', 't': 'Start Date/Time',
+                                    'v': 'Trading Volume', 'vw': 'Volume Weighted Average'})
+
+    # calculations
+    renamed_df.at[0,"Average Price"] = renamed_df["Volume Weighted Average"].mean()
+    renamed_df.at[0,"Highest Overall Price"] = renamed_df['Highest Price'].max()
+    renamed_df.at[0,"Average High"] = renamed_df['Highest Price'].mean()
+    renamed_df.at[0,"Lowest Overall Price"] = renamed_df['Lowest Price'].min()
+    renamed_df.at[0,"Average Low"] = renamed_df['Lowest Price'].mean()
+
+    # Convert time and date
+    renamed_df['Start Date/Time']=(pd.to_datetime(renamed_df['Start Date/Time'],unit='ms'))
+    return renamed_df
+
+def create_csv(ticker_symbol, start_date, end_date, api_key):
+    
+    url = create_url(ticker_symbol, start_date, end_date, api_key)
+    dataframe = create_dataframe(url)
+    dataframe.to_csv(f"{ticker_symbol} Stock prices {start_date} to {end_date}.csv")
+
+def fetch_data():
+    
+    ticker_symbol = ticker_entry.get().upper
+    start_date = start_date_entry.get()
+    end_date = end_date_entry.get()
+    api_key = api_key_entry.get()
+
+    create_csv(ticker_symbol, start_date, end_date, api_key)
+
+root = tk.Tk()
+root.title("Stock Price to CSV")
+
+# Create and place labels and entry widgets for each field
+tk.Label(root, text="Ticker Symbol:").grid(row=0, column=0, padx=10, pady=10)
+ticker_entry = tk.Entry(root)
+ticker_entry.grid(row=0, column=1, padx=10, pady=10)
+
+tk.Label(root, text="Start Date (YYYY-MM-DD):").grid(row=1, column=0, padx=10, pady=10)
+start_date_entry = tk.Entry(root)
+start_date_entry.grid(row=1, column=1, padx=10, pady=10)
+
+tk.Label(root, text="End Date (YYYY-MM-DD):").grid(row=2, column=0, padx=10, pady=10)
+end_date_entry = tk.Entry(root)
+end_date_entry.grid(row=2, column=1, padx=10, pady=10)
+
+tk.Label(root, text="API Key:").grid(row=3, column=0, padx=10, pady=10)
+api_key_entry = tk.Entry(root)
+api_key_entry.grid(row=3, column=1, padx=10, pady=10)
+
+# Create and place the button
+fetch_button = tk.Button(root, text="Fetch Data", command=fetch_data)
+fetch_button.grid(row=4, column=0, columnspan=2, pady=10)
+
+
+
+
+# Run the Tkinter main loop
+root.mainloop()
+
+
+
